15935803463e335650668adb3e2ff0fa0d91fb9c
Pat Palmerston <patpalmerston@gmail.com>
2019-08-19 15:56:21 -0700

    button display and button toggle tests passing

--- webtesting-iii-challenge-master/src/controls/Controls.spec.js
+++ webtesting-iii-challenge-master/src/controls/Controls.spec.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
 
 import Controls from './Controls';
+import Dashboard from '../dashboard/Dashboard';
 
 describe('Controls SnapShot', () => {
 	it('should match Controls SnapShot', () => {
@@ -9,4 +11,31 @@ describe('Controls SnapShot', () => {
 
 		expect(tree.toJSON()).toMatchSnapshot();
 	});
+});
+
+describe('checking for buttons', () => {
+	it('display two toggle buttons', () => {
+		const { getByText } = render(<Controls />);
+
+		getByText('Close Gate');
+		getByText('Lock Gate');
+	});
+});
+
+describe('button text change on toggle', () => {
+	it('Display Open Gate on Closed gate click', () => {
+		const { getByText } = render(<Dashboard />);
+		const closeGateText = getByText('Close Gate');
+		const expected = 'Open Gate'
+		fireEvent.click(closeGateText);
+		expect(closeGateText.textContent).toBe(expected);
+	});
+
+	it('Display Unlock Gate on Lock Gate Click', () => {
+		const { getByText } = render(<Dashboard />);
+		const lockGateText = getByText('Lock Gate');
+		const expected = 'Unlock Gate'
+		fireEvent.click(lockGateText);
+		expect(lockGateText.textContent).toBe(expected);
+	})
 });
