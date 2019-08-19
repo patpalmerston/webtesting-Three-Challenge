9825f78366e4c1902f2b2dbf3b2b4f8b2a234b59
Pat Palmerston <patpalmerston@gmail.com>
2019-08-19 15:14:19 -0700

    red led tests passing

--- webtesting-iii-challenge-master/src/display/Display.spec.js
+++ webtesting-iii-challenge-master/src/display/Display.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+// import { toHaveClass } from '@testing-library/jest-dom'
 
 import Display from './Display';
 
@@ -10,3 +12,41 @@ describe('Display SnapShot', () => {
 		expect(tree.toJSON()).toMatchSnapshot();
 	});
 });
+
+describe('closed prop', () => {
+	it('displays closed if closed prop is === true', () => {
+		const { getByText } = render(<Display closed={true} />);
+		getByText('Closed');
+	});
+
+	it('displays open if closed prop is === false', () => {
+		const { getByText } = render(<Display closed={false} />);
+		getByText('Open');
+	});
+});
+
+describe('locked prop', () => {
+	it('displays Locked if the locked prop is true', () => {
+		const { getByText } = render(<Display locked={true} />);
+		getByText('Locked');
+	});
+
+	it('displays UnLocked if the locked props is false', () => {
+		const { getByText } = render(<Display locked={false} />);
+		getByText('Unlocked');
+	});
+});
+
+describe('red-led class tests on true', () => {
+	it('red-led on locked true', () => {
+		const { container } = render(<Display locked={true} />);
+		expect(container.querySelector('led red-led'));
+	});
+
+	it('red-led on closed true', () => {
+		const { container } = render(<Display closed={true} />);
+		expect(container.querySelector('led red-led'));
+	});
+});
+
+
